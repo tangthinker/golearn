cb5b6efb431787124a3c47c9d175707ce0f4b8e8
shanliao420 <shanliao0420@yeah.net>
2023-12-30 14:14:33 +0800

    1230-learn concurrency

--- concurrency/waitgroup/conc.go
+++ concurrency/waitgroup/conc.go
@@ -1,18 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"github.com/sourcegraph/conc"
-)
-
-func main() {
-	var wg conc.WaitGroup
-	for i := 0; i < 3; i++ {
-		i := i
-		wg.Go(func() {
-			fmt.Println(i)
-		})
-	}
-	wg.Wait()
-	fmt.Println("done.")
-}
+//
+//import (
+//	"fmt"
+//	"github.com/sourcegraph/conc"
+//)
+//
+//func main() {
+//	var wg conc.WaitGroup
+//	for i := 0; i < 3; i++ {
+//		i := i
+//		wg.Go(func() {
+//			fmt.Println(i)
+//		})
+//	}
+//	wg.Wait()
+//	fmt.Println("done.")
+//}
